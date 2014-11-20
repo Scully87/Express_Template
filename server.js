c080779f99b36d0ee222681341c042efce35a8e9
Scully87 <scully87.ts@googlemail.com>
2014-11-20 12:59:04 +0000

    updated file structure ready for use

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express')
 var app = express();
+var expressLayouts = require('express-ejs-layouts');
 //require express (server framework)
-//change the varialbe of express to app so that you can use in the code
+//change the variable of express to app so that you can use in the code
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.static(__dirname + '/public'));
 app.get('/',function(req, res) {
 	// 'req' is request of what/where your "get" is
 	// 'res' is responce, so once its done its get it responds with.......(on next line)
-	res.send("Hello World")
+	res.render("index");
 	// 'res' respond for get, '.send' is send this >> to browser.
 });
 
